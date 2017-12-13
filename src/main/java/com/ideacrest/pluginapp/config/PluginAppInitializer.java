fb2a2dcf19eacec88d1b3f35a5e34e0b086a82a3
Nikhil Pereira <nikhil.pereira@ideacrestsolutions.com>
2017-12-13 13:01:03 +0530

    add web application initializer. Read base package for component from code-analysis-utils properties file

---
+++ src/main/java/com/ideacrest/pluginapp/config/PluginAppInitializer.java
@@ -0,0 +1,41 @@
+package com.ideacrest.pluginapp.config;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.ideacrest.pluginapp.config.WebMvcConfig;
+
+public class PluginAppInitializer  implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		Properties prop = new Properties();
+		InputStreamReader input = null;
+		try {
+			input = new InputStreamReader(getClass().getResourceAsStream("/code-analysis-utils-plugin.properties"));
+			prop.load(input);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Create the dispatcher servlet's Spring application context
+        AnnotationConfigWebApplicationContext dispatcherServlet = new AnnotationConfigWebApplicationContext();
+        dispatcherServlet.register(WebMvcConfig.class);
+        
+        // Register and map the dispatcher servlet
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("code-analysis-utils-dispatcher", new DispatcherServlet(dispatcherServlet));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/code-analysis-utils/*");
+        dispatcherServlet.scan(prop.getProperty("base-package"),"com.ideacrest.pluginapp");
+        dispatcherServlet.getApplicationName();
+	}
+
+}
---
+++ src/main/java/com/ideacrest/pluginapp/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.ideacrest.pluginapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
+	@Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/META-INF/resources/");
+	}
+}
