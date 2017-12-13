c823cc1eb4b1fbde4c0348454f78cbb7c8fc6742
Nikhil Pereira <nikhil.pereira@ideacrestsolutions.com>
2017-12-13 18:28:03 +0530

    add AOP aspectj enabling to web mvc config

--- src/main/java/com/ideacrest/pluginapp/config/WebMvcConfig.java
+++ src/main/java/com/ideacrest/pluginapp/config/WebMvcConfig.java
@@ -3,21 +3,25 @@ package com.ideacrest.pluginapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.ideacrest.pluginapp.aspects.LogAspect;
+
 @Configuration
 @ComponentScan
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter{
 	@Bean
     public ViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
         internalResourceViewResolver.setPrefix("/WEB-INF/views/");
-        internalResourceViewResolver.setSuffix(".html");
+        internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
     }
 	
@@ -25,4 +29,5 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
 	    registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/META-INF/resources/");
 	}
+	
 }
